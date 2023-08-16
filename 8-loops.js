bca14b9dc79fa755d44554c5d957402c77abd2cd
Elizabethcristina10 <elizabethcristina2908@gmail.com>
2023-08-15 21:41:59 -0300

    feito

--- 8-loops.js
+++ 8-loops.js
@@ -18,16 +18,31 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
+let destinoExiste = false;
+
+// usando o while
 
 while(contador<3){
-
     if(listaDeDestinos[contador] == destino){
-        console.log("Destino existe");
-    }else{
-        console.log("Destino não existe");
+        destinoExiste = true;
+        break;
     }
-
-
     contador += 1;
 }
 
+console.log("Destino existe:", destinoExiste);
+
+if (podeComprar && destinoExiste){
+    console.log("Boa Viagem!");
+}else {
+    console.log("Desculpe tivemos um erro!");
+}
+
+// usando o For
+
+for(let i = 0; i < 3 ; i++){
+    if(listaDeDestinos[i] == destino){
+        destinoExiste = true;
+   }
+    
+}
